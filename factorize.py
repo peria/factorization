9e68be367dfea96eb9cbe1911a4217ea3547c5c5
Hitoshi Yoshida <git@peria.jp>
2014-02-09 00:25:06 +0900

    add Millar-Rabin test and check if n is prime.

--- factorize.py
+++ factorize.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python
 
+import prime
 import rho
 import p_one
 import sys
 import trial
 
 if __name__ == "__main__":
-  n0 = n1 = n2 = nn = 0
+  n0 = n1 = n2 = np = 0
   for line in sys.stdin:
     line = line.strip()
 
     n = int(line)
-    (p0, n) = trial.factor(n)
-    (p1, n) = p_one.factor(n)
-    (p2, n) = rho.factor(n)
-
-    if len(p0) > 0:
+    (p, n) = trial.factor(n)
+    if len(p) > 0:
       n0 += 1
-    elif len(p1) > 0:
-      n1 += 1
-    elif len(p2) > 0:
-      n2 += 1
-    else:
-      nn += 1
-  print n0, n1, n2, nn
+    if not prime.IsPrime(n):
+      (q, n) = p_one.factor(n)
+      if len(q) > 0:
+        n1 += 1
+      p.extend(q)
+    if not prime.IsPrime(n):
+      (q, n) = rho.factor(n)
+      if len(q) > 0:
+        n2 += 1
+      p.extend(q)
+    if prime.IsPrime(n):
+      print "%s: %s * %d" % (line, p, n)
+      np += 1
+
+  print n0, n1, n2, np
